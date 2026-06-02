acd40e300dca52db334782868739502024f3fd1b
Abhyudaya Yadav <160077330+abhyudayayadav07@users.noreply.github.com>
2026-06-02 13:24:00 +0530

    Delete test_binance.py

--- test_binance.py
+++
@@ -1,9 +0,0 @@
-from config import API_KEY, API_SECRET
-from binance.client import Client
-
-print(API_KEY is not None)
-print(API_SECRET is not None)
-
-client = Client(API_KEY, API_SECRET)
-
-print("Client created successfully")
